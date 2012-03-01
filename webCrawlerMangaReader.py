1d31c93413b978bcd8b363e0788fe0c255ade3ac
Filipe Cifali Stangler <cifali.filipe@gmail.com>
2012-03-01 15:00:48 -0300

    Removed some uneeded stuff
    Refactored downloadImage and downloadChapter, merging into listCreator, accepting 2 parameters to creat each list.

--- webCrawlerMangaReader.py
+++ webCrawlerMangaReader.py
@@ -5,60 +5,41 @@ from urllib import urlretrieve
 import os
 from BeautifulSoup import BeautifulSoup
 
-# Configuration for downloads
-base_url = 'http://mangareader.net' # The site, do NOT change it, if you don' know what you are doing!
-target = 'air-gear' # Change to whatever manga you need :)
-user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)' # Let's fake a Firefox ;)
-headers = { 'User-Agent' : user_agent } # Keep it trolling.
-directory = '/Users/filipecifalistangler/Downloads/manga/' # Change to whatever directory you want to save everything.
-startFrom = 338 # If the download should start from a precise chapter(default is 0, sometimes, -1 is needed, depending
+base_url = 'http://mangareader.net'
+target = 'one-piece' # Change to whatever manga you need :)
+user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
+headers = { 'User-Agent' : user_agent }
+directory = '/Users/filipecifalistangler/Downloads/manga/'
+startFrom = 0 # If the download should start from a precise chapter(default is 0, sometimes, -1 is needed, depending
 # on the page itself and the links).
 
-# Necessary lists for management of data
 urlChapters = []
-urlChapter = []
-urlImage = []
+listUrl = []
+listUrl2 = []
 
 request = urllib2.Request(base_url+'/'+target, headers = headers)
 response = urllib2.urlopen(request)
 document = response.read()
-soup = BeautifulSoup(document) # BeautifulSoup parse HTML in a beautiful way :)
-links = soup.findAll('a') # Now we make a list with all Links on the page of the manga
+soup = BeautifulSoup(document)
+links = soup.findAll('a')
 
-# New we make the list so we can later do a loop to get it all
 for link in links:
     urlChapters.append(base_url+link['href'])
 
-def downloadChapter(url):
-    '''
-    Creates the list of all links on the selected chapter so we can later parse the images.
-    '''
-    urlChap = url
-    requestChap = urllib2.Request(urlChap, headers = headers)
-    responseChap = urllib2.urlopen(requestChap)
-    documentChap = responseChap.read()
-    soupChap = BeautifulSoup(documentChap)
-    linksChap = soupChap.findAll('option')  
-    for link in linksChap:
-        urlChapter.append(link['value'])
-
-def downloadImage(url):
-    '''
-    Creates the list of all the URIs from the images, so we can later download them.
-    '''
-    urlImg = url
-    requestImg = urllib2.Request(urlImg, headers = headers)
-    responseImg = urllib2.urlopen(requestImg)
-    documentImg = responseImg.read()
-    soupImg = BeautifulSoup(documentImg)
-    linksImg = soupImg.findAll('img')
-    for link in linksImg:
-        urlImage.append(link['src'])
+def listCreator(url, type, grab):
+    gUrl = url
+    grequest = urllib2.Request(gUrl, headers = headers)
+    gresponse = urllib2.urlopen(grequest)
+    gdocument = gresponse.read()
+    gsoup = BeautifulSoup(gdocument)
+    glinks = gsoup.findAll('%s' %type)
+    for link in glinks:
+        if type == 'option':
+            listUrl.append(link['%s' %grab])
+        else:
+            listUrl2.append(link['%s' %grab])
 
 def createDir(url):
-    '''
-    We need to check if the directory exists before trying to create them.
-    '''
     name = url
     splitedName = name.split('/')
     dir = directory+target+'/'
@@ -69,39 +50,27 @@ def createDir(url):
         os.mkdir(dirChapter)
     return dirChapter
 
-'''
-A little "work-around" here, so we can "skip" non-related links and get only chapter links. The 20 first links are from
-the header of the site, the last 8 are from the footer.
-'''
-
 startFrom += 20
 finalUrls = urlChapters[startFrom:]
 final = finalUrls[:(len(finalUrls)-8)]
 
-# And then we do the loop.
 for list in final:
-    # Let's get a return so we can use as a save point for the file.
     dirTarget = createDir(list)
-    urlChapter = []
-    # We now get the list of urls from the Chapter and loop again inside them.
-    downloadChapter(list)
-    for urls in urlChapter:
-        urlImage = []
-        # Here we grab the image URI and create a complete URL
-        downloadImage(base_url+urls)
-        # Defines where it will be stored
-        saveDir = os.path.join(dirTarget, urlImage[-1].split('/')[-1])
+    listUrl = []
+    listCreator(list, 'option', 'value')
+    for urls in listUrl:
+        listUrl2 = []
+        listCreator(base_url+urls, 'img', 'src')
+        saveDir = os.path.join(dirTarget, listUrl2[-1].split('/')[-1])
         if not os.path.exists(saveDir):
-            # And so we download it!
-            urlretrieve(urlImage[-1], saveDir)
-            print urlImage[-1], "saved in:", saveDir
+            urlretrieve(listUrl2[-1], saveDir)
+            print listUrl2[-1], "saved in:", saveDir
         else:
             # Check if the download it's a Zero-Sized / Broken file.
-            if int(urllib.urlopen(urlImage[-1]).info()['Content-Length']) == os.stat(saveDir).st_size:
-                # Let's not download again right?
-                print urlImage[-1], "Already downloaded, skipping to next..."
+            if int(urllib.urlopen(listUrl2[-1]).info()['Content-Length']) == os.stat(saveDir).st_size:
+                print listUrl2[-1], "Already downloaded, skipping to next..."
             else:
-                print "Online Length:", int(urllib.urlopen(urlImage[-1]).info()['Content-Length'])
+                print "Online Length:", int(urllib.urlopen(listUrl2[-1]).info()['Content-Length'])
                 print "Local Length:", os.stat(saveDir).st_size
-                urlretrieve(urlImage[-1], saveDir)
-                print urlImage[-1], "downloading again, the size of the file differs from our."
+                urlretrieve(listUrl2[-1], saveDir)
+                print listUrl2[-1], "downloading again, the size of the file differs from our."
